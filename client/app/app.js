679f93e94f98b507d8d46963429e676613ae5083
Victor Gomez <vimagox@gmail.com>
2015-12-08 21:47:59 -0700

    Initial commit

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('xApp', [
+  'xApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
