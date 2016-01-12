b72777be8d2e9e944fdc53a9cd6352be94b4507d
Victor Gomez <vimagox@gmail.com>
2016-01-12 14:58:17 -0700

    Adding more nav

--- client/app/govs/govs.js
+++ client/app/govs/govs.js
@@ -74,38 +74,57 @@ angular.module('xApp.govs', ['ui.router'])
 
     $scope.states = [
       {id:0,  uid:'fed', name:"Gobierno Federal", vato:"Enrique Peña Nieto", period:"2012 - 2018", party:"pri"},
-      {id:1,  uid:'agu', name:"Aguascalientes", vato:"Carlos Lozano de la Torre", period:"2010 - 2016", party:"pri"},
-      {id:2,  uid:'bcn', name:"Baja California", vato:"Francisco Vega de Lamadrid", period:"2013 - 2019", party:"pan"},
-      {id:3,  uid:'bcs', name:"Baja California Sur", vato:"Marco Alberto Covarrubias Villaseñor", period:"2011 - 2017", party:"pan"},
-      {id:4,  uid:'cam', name:"Campeche", vato:"Alejandro Moreno Cárdenas", period:"2015 - 2021", party:"pri"},
-      {id:5,  uid:'cha', name:"Chiapas", vato:"Manuel Velasco Coello", period:"2012 - 2018", party:"pvem"},
-      {id:6,  uid:'chi', name:"Chihuahua", vato:"César Horacio Duarte Jáquez", period:"2010 - 2016", party:"pri"},
-      {id:7,  uid:'coa', name:"Coahuila", vato:"Rubén Moreira Valdez", period:"2011 - 2017", party:"pri"},
-      {id:8,  uid:'col', name:"Colima", vato:"Mario Anguiano Moreno / Ramón Pérez Díaz", period:"2009 - 2015", party:"pri"},
-      {id:9,  uid:'dur', name:"Durango", vato:"Jorge Alejandro Salum del Palacio", period:"2010 - 2016", party:"pri"},
-      {id:10,  uid:'gua', name:"Guanajuato", vato:"Miguel Márquez Márquez", period:"2012-2018", party:"pan"},
-      {id:11, uid:'gue', name:"Guerrero", vato:"Héctor Antonio Astudillo Flores", period:"2015-2021", party:"pri"},
-      {id:12, uid:'gue', name:"Hidalgo", vato:"Francisco Olvera Ruiz", period:"2011 - 2016", party:"pri"},
-      {id:13, uid:'jal', name:"Jalisco", vato:"Aristóteles Sandoval", period:"2013 - 2019", party:"pri"},
-      {id:14, uid:'mex', name:"México", vato:"Eruviel Avila Villegas", period:"2011 - 2017", party:"pri"},
-      {id:15, uid:'mic', name:"Michoacán", vato:"Silvano Aureoles Conejo", period:"2015 - 2021", party:"prd"},
-      {id:16, uid:'mor', name:"Morelos", vato:"Graco Ramírez", period:"2012 - 2018", party:"prd"},
-      {id:17, uid:'nay', name:"Nayarit", vato:"Roberto Sandoval", period:"2011 - 2017", party:"pri"},
-      {id:18, uid:'nvl', name:"Nuevo León", vato:"Jaime Heliodoro Rodríguez Calderón", period:"2015 - 2021", party:"ind"},
-      {id:19, uid:'oax', name:"Oaxaca", vato:"Gabino Cué Monteagudo", period:"2010 - 2016", party:"pop"},
-      {id:20, uid:'pue', name:"Puebla", vato:"Rafael Moreno Valle", period:"2011 - 2017", party:"pan"},
-      {id:21, uid:'que', name:"Querétaro", vato:"Francisco Domínguez Servién", period:"2015 - 2021", party:"pan"},
-      {id:22, uid:'qui', name:"Quintana Roo", vato:"Roberto Borge Angulo", period:"2011 - 2016", party:"pri"},
+      {id:1,  uid:'aguas', name:"Aguascalientes", vato:"Carlos Lozano de la Torre", period:"2010 - 2016", party:"pri"},
+      {id:2,  uid:'baja', name:"Baja California", vato:"Francisco Vega de Lamadrid", period:"2013 - 2019", party:"pan"},
+      {id:3,  uid:'bajasur', name:"Baja California Sur", vato:"Marco Alberto Covarrubias Villaseñor", period:"2011 - 2017", party:"pan"},
+      {id:4,  uid:'campeche', name:"Campeche", vato:"Alejandro Moreno Cárdenas", period:"2015 - 2021", party:"pri"},
+      {id:5,  uid:'chiapas', name:"Chiapas", vato:"Manuel Velasco Coello", period:"2012 - 2018", party:"pvem"},
+      {id:6,  uid:'chihuahua', name:"Chihuahua", vato:"César Horacio Duarte Jáquez", period:"2010 - 2016", party:"pri"},
+      {id:7,  uid:'coahuila', name:"Coahuila", vato:"Rubén Moreira Valdez", period:"2011 - 2017", party:"pri"},
+      {id:8,  uid:'colima', name:"Colima", vato:"Mario Anguiano Moreno / Ramón Pérez Díaz", period:"2009 - 2015", party:"pri"},
+      {id:9,  uid:'durango', name:"Durango", vato:"Jorge Alejandro Salum del Palacio", period:"2010 - 2016", party:"pri"},
+      {id:10,  uid:'guanajuato', name:"Guanajuato", vato:"Miguel Márquez Márquez", period:"2012-2018", party:"pan"},
+      {id:11, uid:'guerrero', name:"Guerrero", vato:"Héctor Antonio Astudillo Flores", period:"2015-2021", party:"pri"},
+      {id:12, uid:'hidalgo', name:"Hidalgo", vato:"Francisco Olvera Ruiz", period:"2011 - 2016", party:"pri"},
+      {id:13, uid:'jalisco', name:"Jalisco", vato:"Aristóteles Sandoval", period:"2013 - 2019", party:"pri"},
+      {id:14, uid:'mexico', name:"México", vato:"Eruviel Avila Villegas", period:"2011 - 2017", party:"pri"},
+      {id:15, uid:'michoacan', name:"Michoacán", vato:"Silvano Aureoles Conejo", period:"2015 - 2021", party:"prd"},
+      {id:16, uid:'morelos', name:"Morelos", vato:"Graco Ramírez", period:"2012 - 2018", party:"prd"},
+      {id:17, uid:'nayarit', name:"Nayarit", vato:"Roberto Sandoval", period:"2011 - 2017", party:"pri"},
+      {id:18, uid:'nuevoleon', name:"Nuevo León", vato:"Jaime Heliodoro Rodríguez Calderón", period:"2015 - 2021", party:"ind"},
+      {id:19, uid:'oaxaca', name:"Oaxaca", vato:"Gabino Cué Monteagudo", period:"2010 - 2016", party:"pop"},
+      {id:20, uid:'puebla', name:"Puebla", vato:"Rafael Moreno Valle", period:"2011 - 2017", party:"pan"},
+      {id:21, uid:'queretaro', name:"Querétaro", vato:"Francisco Domínguez Servién", period:"2015 - 2021", party:"pan"},
+      {id:22, uid:'roo', name:"Quintana Roo", vato:"Roberto Borge Angulo", period:"2011 - 2016", party:"pri"},
       {id:23, uid:'slp', name:"San Luis Potosí", vato:"Fernando Toranzo Fernández", period:"2009 - 2015", party:"pri"},
-      {id:24, uid:'sin', name:"Sinaloa", vato:"Mario López Valdez", period:"2011 - 2016", party:"pan"},
-      {id:25, uid:'son', name:"Sonora", vato:"Claudia Pavlovich Arellano", period:"2015 - 2021", party:"pri"},
-      {id:26, uid:'tab', name:"Tabasco", vato:"Arturo Núńez Jiménez", period:"2012 - 2018", party:"prd"},
-      {id:27, uid:'tam', name:"Tamaulipas", vato:"Egidio Torre Cantú", period:"2011 - 2016", party:"pri"},
-      {id:28, uid:'tla', name:"Tlaxcala", vato:"Mariano González Zarur", period:"2011 - 2017", party:"pri"},
-      {id:29, uid:'ver', name:"Veracruz", vato:"Javier Duarte de Ochoa", period:"2010 - 2016", party:"pri"},
-      {id:30, uid:'yuc', name:"Yucatán", vato:"Rolando Zapata Bello", period:"2012 - 2018", party:"pri"},
-      {id:31, uid:'zac', name:"Zacatecas", vato:"Miguel Alonso Reyes", period:"2010 - 2016", party:"pri"},
-      {id:32, uid:'df',  name:"D.F.", vato:"Miguel Angel Mancera Espinosa", period:"2012 - 2018", party:"prd"}
+      {id:24, uid:'sinaloa', name:"Sinaloa", vato:"Mario López Valdez", period:"2011 - 2016", party:"pan"},
+      {id:25, uid:'sonora', name:"Sonora", vato:"Claudia Pavlovich Arellano", period:"2015 - 2021", party:"pri"},
+      {id:26, uid:'tabasco', name:"Tabasco", vato:"Arturo Núńez Jiménez", period:"2012 - 2018", party:"prd"},
+      {id:27, uid:'tamaulipas', name:"Tamaulipas", vato:"Egidio Torre Cantú", period:"2011 - 2016", party:"pri"},
+      {id:28, uid:'tlaxcala', name:"Tlaxcala", vato:"Mariano González Zarur", period:"2011 - 2017", party:"pri"},
+      {id:29, uid:'veracruz', name:"Veracruz", vato:"Javier Duarte de Ochoa", period:"2010 - 2016", party:"pri"},
+      {id:30, uid:'yucatan', name:"Yucatán", vato:"Rolando Zapata Bello", period:"2012 - 2018", party:"pri"},
+      {id:31, uid:'zacatecas', name:"Zacatecas", vato:"Miguel Alonso Reyes", period:"2010 - 2016", party:"pri"},
+      {id:32, uid:'df',  name:"D.F.", vato:"Miguel Angel Mancera Espinosa", period:"2012 - 2018", party:"prd",
+        municipios: [
+          {id:3200, uid:'ao',  name:"Alvaro Obregón", vato:"Maria Antonieta Hidalgo Torres", period:"", party:"pri"},
+          {id:3201, uid:'az',  name:"Azcapotzalco", vato:"Pablo Moctezuma Barragán", period:"", party:"pri"},
+          {id:3202, uid:'bj',  name:"Benito Juárez", vato:"Christian von Roehrich", period:"", party:"pri"},
+          {id:3203, uid:'co',  name:"Coyoacán", vato:"José Valentin Maldonado Salgado", period:"", party:"pri"},
+          {id:3204, uid:'cj',  name:"Cuajimalpa", vato:"Miguel Angel Salazar", period:"", party:"pan"},
+          {id:3205, uid:'cu',  name:"Cuauhtémoc", vato:"Ricardo Monreal", period:"", party:"pan"},
+          {id:3206, uid:'gu',  name:"Gustavo A. Madero", vato:"Victor Hugo Lobo", period:"", party:"prd"},
+          {id:3207, uid:'ic',  name:"Iztacalco", vato:"Carlos Estrada", period:"", party:"prd"},
+          {id:3208, uid:'ip',  name:"Iztapalapa", vato:"Dione Anguiano", period:"", party:"pri"},
+          {id:3209, uid:'mc',  name:"Magdalena Contreras", vato:"Fernando Mercado", period:"", party:"pri"},
+          {id:3210, uid:'mh',  name:"Miguel Hidalgo", vato:"Xóchitl Gálvez", period:"", party:"pan"},
+          {id:3211, uid:'ma',  name:"Milpa Alta", vato:"Jorge Alvarado Galicia", period:"", party:"pan"},
+          {id:3212, uid:'th',  name:"Tláhuac", vato:"Rigoberto Salgado", period:"", party:"pri"},
+          {id:3213, uid:'tp',  name:"Tlalpan", vato:"Claudia Sheinbaum Pardo", period:"", party:"pri"},
+          {id:3214, uid:'ve',  name:"Venustiano Carranza", vato:"Israel Moreno Rivera", period:"", party:"pri"},
+          {id:3215, uid:'xo',  name:"Xochimilco", vato:"Avelino Méndez Rangel", period:"", party:"prd"}
+        ]
+      }
     ];
   }
 ])
@@ -139,5 +158,11 @@ angular.module('xApp.govs', ['ui.router'])
 .controller('XMunicipiosCtrl', ['$scope', '$state', 'xStorage',
   function ($scope, $state, xStorage) {
     $scope.xstate = xStorage.get('xstate');
+
+    $scope.loadMunicipio = function(municipioId) {
+      var x = $scope.xstate.municipios[municipioId-3200];
+      xStorage.put('xstate', x);
+      $state.go('xstate', {xstate: x.uid});
+    };
   }
 ]);
